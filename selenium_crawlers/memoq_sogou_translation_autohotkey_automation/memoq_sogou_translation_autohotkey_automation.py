cf1a3b9032da40ca891d7a85646e47f897712230
Jim132 <526554176@qq.com>
2022-06-08 12:51:59 +0800

    memoQ automation added.

--- selenium_crawlers/memoq_sogou_translation_autohotkey_automation/memoq_sogou_translation_autohotkey_automation.py
+++ selenium_crawlers/memoq_sogou_translation_autohotkey_automation/memoq_sogou_translation_autohotkey_automation.py
@@ -14,35 +14,49 @@ driver.get('https://fanyi.sogou.com/')
 ahk = AHK()
 windows_list = list(ahk.windows())
 print(windows_list)
-trados_window = None
+memoq_window = None
 for window in windows_list:
     print(str(window.get_title().decode('gbk')))
-    if str(window.get_title().decode('gbk')).startswith('SDL Trados Studio - '):
-        trados_window = window
+    if str(window.get_title().decode('gbk')).startswith('memoQ - '):
+        memoq_window = window
         break
-if trados_window is None:
-    raise Exception('SDL Trados Studio is not opened.')
+if memoq_window is None:
+    raise Exception('memoQ is not opened.')
 
 current_time = 0
 while True:
     current_time += 1
     if current_time > copy_times:
         break
-    if trados_window is not None:
-        trados_window.activate()
-        ahk.send('!{Insert}')
+    if memoq_window is not None:
+        memoq_window.activate()
+        ahk.send('^+s')
+        sleep(1.2)
         ahk.send('^a')
+        sleep(0.2)
         ahk.send('^c')
 
+    button_clear_element = None
+    try:
+        button_clear_element = driver.find_element(by=By.CSS_SELECTOR, value='.trans-con>.btn-clear')
+    except Exception as e:
+        print(e)
+        print("Clear text button not found.")
+    try:
+        button_clear_element.click()
+    except Exception as e:
+        print(e)
+        print("Clear text button somehow cannot be clicked.")
+    sleep(0.2)
     sogou_input_textarea_element = driver.find_element(by=By.CSS_SELECTOR, value='#trans-input')
     sogou_input_textarea_element.click()
     sogou_input_textarea_element.send_keys(Keys.CONTROL, 'a')
     sogou_input_textarea_element.send_keys(Keys.CONTROL, 'v')
-    sleep(1.2)
+    sleep(2.0)
     copy_button_element = driver.find_element(by=By.CSS_SELECTOR, value='.btn-copy')
     copy_button_element.click()
 
-    if trados_window is not None:
-        trados_window.activate()
+    if memoq_window is not None:
+        memoq_window.activate()
         ahk.send('^v')
         ahk.send('{Down}')
---
+++ self_learn_folder/2_do_other_peoples_homework/spyder_2022_05_11/1_math.py
@@ -0,0 +1,14 @@
+# 学籍番号と名前
+
+import math
+
+for i in range(1, 6):
+    print(math.sqrt(i))
+print()
+
+for i in range(0, 5):
+    print(math.exp(i))
+print()
+
+area = math.pi * (10 ** 2)
+print("{:.2f}".format(area))

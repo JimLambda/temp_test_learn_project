2a322a0171de22bad5fa0d714b978b7854861c78
SuperJim <526554176@qq.com>
2023-06-26 20:31:08 +0900

    Bug fix. empty_house_title may be null.

--- selenium_crawlers/citymobile_co_jp_crawler/citymobile_co_jp_crawler.py
+++ selenium_crawlers/citymobile_co_jp_crawler/citymobile_co_jp_crawler.py
@@ -23,36 +23,40 @@ def get_all_room_info():
                 title_for_clicking = li_element.find_element(by=By.XPATH, value='.//div[@class="bukkenName"]/a')
                 title_for_clicking.click()
 
-                empty_house_title = driver.find_element(by=By.XPATH,
-                                                        value='//dt[@class="article-list-title" and contains(text(), "【空室")]')
-                parent_element = empty_house_title.find_element(by=By.XPATH, value='./..')
-
-                # If there is "more" button, click it.
-                # (If the building has more than 10 empty rooms, there will be a more button.)
+                # If there is no "【空室" title, just skip the following procedures and go to next li_element.
                 try:
-                    more_button = parent_element.find_element(by=By.XPATH,
-                                                              value='.//a[contains(@class, "btnListMore")]')
-                    more_button.click()
+                    empty_house_title = driver.find_element(by=By.XPATH,
+                                                            value='//dt[@class="article-list-title" and contains(text(), "【空室")]')
+                    parent_element = empty_house_title.find_element(by=By.XPATH, value='./..')
+
+                    # If there is "more" button, click it.
+                    # (If the building has more than 10 empty rooms, there will be a more button.)
+                    try:
+                        more_button = parent_element.find_element(by=By.XPATH,
+                                                                  value='.//a[contains(@class, "btnListMore")]')
+                        more_button.click()
+                    except selenium.common.exceptions.NoSuchElementException:
+                        pass
+
+                    rent_fee_elements_list = parent_element.find_elements(by=By.XPATH, value='.//div[@class="rentFee"]')
+                    if len(rent_fee_elements_list) == 0:
+                        driver.back()
+                        continue
+                    manage_fee_elements_list = parent_element.find_elements(by=By.XPATH, value='.//div[@class="manageFee"]')
+                    building_name_element = driver.find_element(by=By.XPATH, value='//h2[@class="article-name"]')
+
+                    for index in range(0, len(rent_fee_elements_list)):
+                        room_info_list = [building_name_element.text, rent_fee_elements_list[index].text,
+                                          manage_fee_elements_list[index].text]
+                        for room_info_list_index in range(1, 3):
+                            room_info_list[room_info_list_index] = room_info_list[room_info_list_index].replace("円",
+                                                                                                                "").replace(
+                                ",",
+                                "").strip()
+                        file.write("{},{},{}\n".format(room_info_list[0], room_info_list[1], room_info_list[2]))
+                        file.flush()
                 except selenium.common.exceptions.NoSuchElementException:
                     pass
-
-                rent_fee_elements_list = parent_element.find_elements(by=By.XPATH, value='.//div[@class="rentFee"]')
-                if len(rent_fee_elements_list) == 0:
-                    driver.back()
-                    continue
-                manage_fee_elements_list = parent_element.find_elements(by=By.XPATH, value='.//div[@class="manageFee"]')
-                building_name_element = driver.find_element(by=By.XPATH, value='//h2[@class="article-name"]')
-
-                for index in range(0, len(rent_fee_elements_list)):
-                    room_info_list = [building_name_element.text, rent_fee_elements_list[index].text,
-                                      manage_fee_elements_list[index].text]
-                    for room_info_list_index in range(1, 3):
-                        room_info_list[room_info_list_index] = room_info_list[room_info_list_index].replace("円",
-                                                                                                            "").replace(
-                            ",",
-                            "").strip()
-                    file.write("{},{},{}\n".format(room_info_list[0], room_info_list[1], room_info_list[2]))
-                    file.flush()
 
                 driver.back()
 
